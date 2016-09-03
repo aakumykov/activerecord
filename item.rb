c1a7306d767cc36c2e5d6fa0ffb99a671f8e4184
Андрей Кумыков <aakumykov@yandex.ru>
2016-09-03 14:15:31 +0700

    пробы с POST

--- item.rb
+++ item.rb
@@ -1,10 +1,28 @@
 require 'grape'
+require_relative 'model'
 
 class ItemAPI < Grape::API
 	format :json
 
 	get '/' do
 		{ main:'page' }
+	end
+
+	[:list,:items].each do |name|
+		get name do
+			Item.all
+		end
+	end
+
+	resource :items do
+		params do
+			requires :title
+			requires :content
+		end
+		
+		post :new do
+			{ params[:title].count.to_s => params[:content].count }
+		end
 	end
 
 end
